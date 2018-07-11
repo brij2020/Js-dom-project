b9ac213d6bedf7900015137e644ac3399a205374
brijbhan <brijphy@gmail.com>
2018-07-11 13:16:18 +0530

    final commit

--- app.js
+++ app.js
@@ -1,39 +1,81 @@
-console.log("hello, js");
+
 
 // UI variables 
 
-const foo = document.getElementById("foo");
-const task_list = document.querySelector(".collection");
-const filter = document.getElementById("filter");
-const taskInput = document.getElementById("task");
-const clearButton = document.getElementById("clear-task");
+const foo           = document.getElementById("foo");
+const task_list     = document.querySelector(".collection");
+const filter        = document.getElementById("filter");
+const taskInput     = document.getElementById("task");
+const clearButton   = document.getElementById("btn");
+
+//Event listeners
 taskInput.addEventListener('focus',fun2);
-function fun2(){
+task_list.addEventListener('click',cls);
+clearButton.addEventListener('click',task_clear);
+foo.addEventListener('click',fun);
+
+// implementation
+
+function fun2()
+{
     taskInput.addEventListener('blur',function(){
         
-        if(taskInput.value == ""){
+        if(taskInput.value == "") {
             foo.disabled = true;
-        } else{
+        } else {
             foo.disabled = false;
         }
 
     })
     
 }
-foo.addEventListener('click',fun);
-function fun(e){
-    e.preventDefault();
-    
-    li = document.createElement('li');
-    li.className = "collection-item ";
-    li.appendChild(document.createTextNode(taskInput.value));
-    remIcon = document.createElement('a');
-    remIcon.className = "delete-item secondary-content";
-    remIcon.innerHTML='<i class="fa fa-remove"></i>';
-    li.appendChild(remIcon); 
-    task_list.appendChild(li);
+
+function fun(e)
+{
+
+    li = document.createElement('li');//creating element 
+    li.className = "collection-item ";//assign class name
+    li.appendChild(document.createTextNode(taskInput.value));//creating text node and assign append to li
+    remIcon = document.createElement('a');//anchor tag create
+    remIcon.className = "delete-item secondary-content";//class name 
+    remIcon.innerHTML='<i class="fa fa-remove "></i>';//add innerHTML
+    li.appendChild(remIcon); //append to li
+    task_list.appendChild(li);//lastly li append to ul
     taskInput.value = "";
-    
+    e.preventDefault();
     
 
 }
+
+
+function cls(event)
+{
+   
+   if(event.target.parentElement.classList.contains("delete-item")){
+       if(confirm('Are you sur?')) {
+        event.target.parentElement.parentElement.remove();
+       }
+   }
+}
+
+function task_clear(event) 
+{
+    while (task_list.firstChild) {
+        task_list.removeChild(task_list.firstChild);
+    }
+    
+}
+
+filter.addEventListener('keyup',(event)=>{
+
+    text = (event.target.value);
+    document.querySelectorAll(".collection-item").forEach((task_list)=>{
+        if(task_list.firstChild.textContent.indexOf(text)!=-1){
+           task_list.style.display = 'block'; 
+        } else {
+            task_list.style.display = 'none';
+        }
+        
+    });
+
+})
