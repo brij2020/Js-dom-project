e4f1828ff51b6e141462f94945a1f93c024e6042
brijbhan <brijphy@gmail.com>
2018-07-11 19:09:34 +0530

    finishing all work

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ taskInput.addEventListener('focus',disable_add_task_button);
 task_list.addEventListener('click',clear_task);
 clearButton.addEventListener('click',all_task_clear);
 foo.addEventListener('click',create_new_task);
-
+document.addEventListener('DOMContentLoaded',saved_data);
 // implementation
 
 function disable_add_task_button()
@@ -26,37 +26,32 @@ function disable_add_task_button()
             foo.disabled = false;
         }
 
-    })
+    });
     
 }
 
 function create_new_task(e)
-{
-    e.preventDefault();    
+{      
     if(taskInput.value == ''){
         //do nothing
     } else {
-    li = document.createElement('li');//creating element 
-    li.className = "collection-item ";//assign class name
-    li.appendChild(document.createTextNode(taskInput.value));//creating text node and assign append to li
-    remIcon = document.createElement('a');//anchor tag create
-    remIcon.className = "delete-item secondary-content";//class name 
-    remIcon.innerHTML='<i class="fa fa-remove "></i>';//add innerHTML
-    li.appendChild(remIcon); //append to li
-    task_list.appendChild(li);//lastly li append to ul
+    
+    add_collection(taskInput.value);
+    storageTaskInLocalStorage(taskInput.value);
     taskInput.value = "";
-    
-    
-    }
-    
+    }   
 }
 
-
 function clear_task(event)
-{
-   
+{ 
    if(event.target.parentElement.classList.contains("delete-item")){
+
        if(confirm('Are you sur?')) {
+        tasks = []
+        ele = localStorage.getItem('tasks');
+        tasks = JSON.parse(ele);
+        tasks.splice(tasks.indexOf(event.target.parentElement.parentElement.textContent),1);
+        localStorage.setItem('tasks',JSON.stringify(tasks));
         event.target.parentElement.parentElement.remove();
        }
    }
@@ -67,7 +62,7 @@ function all_task_clear(event)
     while (task_list.firstChild) {
         task_list.removeChild(task_list.firstChild);
     }
-    
+    localStorage.removeItem('tasks');
 }
 
 filter.addEventListener('keyup',(event)=>{
@@ -82,3 +77,38 @@ filter.addEventListener('keyup',(event)=>{
     });    
 
 });
+
+function storageTaskInLocalStorage(task)
+{
+    let tasks;
+    if(localStorage.getItem('tasks') === null) {
+        localStorage.removeItem('tasks');
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+    
+}
+function saved_data(event) 
+{   
+    task = localStorage.getItem('tasks');
+    task = JSON.parse(task);
+    if(task === null){
+        //don't so anything 
+    } else {
+         task.forEach((task)=>add_collection(task));    
+    }
+}
+function add_collection(el){
+
+    li = document.createElement('li');//creating element 
+    li.className = "collection-item ";//assign class name
+    li.appendChild(document.createTextNode(el));//creating text node and assign append to li
+    remIcon = document.createElement('a');//anchor tag create
+    remIcon.className = "delete-item secondary-content";//class name 
+    remIcon.innerHTML='<i class="fa fa-remove "></i>';//add innerHTML
+    li.appendChild(remIcon); //append to li
+    task_list.appendChild(li);//lastly li append to ul
+}
---
+++ test.js
@@ -0,0 +1,13 @@
+
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+  
+    tasks.push(task);
+  
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
